f62c869652e9274d666ae8deddb359178e655338
snehalmathure <snehalmathure06@gmail.com>
2024-09-16 23:53:50 +0530

    assignmnet 4

---
+++ Assignmnet 4/assign4_1.cpp
@@ -0,0 +1,87 @@
+#include<iostream>
+using namespace std;
+class time{
+    int h;
+    int m;
+    int s;
+    public:
+    time(){
+        h=1;
+        m=1;
+        s=1;
+    }
+    time(int h,int m, int s)
+    {
+        this->h=h;
+        this->m=m;
+        this->s=s;
+    }
+    void setHour(int h){
+        this->h=h;
+
+    }
+    void setMinutes(int m){
+        this->m=m;
+    }
+    void setSeconds(int s){
+        this->s=s;
+    }
+    int getHour()
+    {
+        return h;
+    }
+    int getMinutes()
+    {
+        return m;
+    }
+    int getSeconds(){
+        return s;
+    }
+    void accept()
+    {
+        cout<<"Enter hour: "<<endl;
+        cin>>h;
+        cout<<"Enter minutes: "<<endl;
+        cin>>m;
+        cout<<"Enter seconds: "<<endl;
+        cin>>s;
+    }
+    void display()
+    {
+        cout<<h<<":"<<m<<":"<<s<<endl;
+    }
+};
+int main()
+{
+    time **tptr=new time*[5];
+    for(int i=0; i<6;i++)
+    {
+
+    tptr[i]=new time;
+        tptr[i]->accept();
+    }
+        // tptr[1]->accept();
+        // tptr[2]->accept();
+        // tptr[3]->accept();
+
+        // tptr[4]->accept();
+        
+for(int i=0; i<5; i++)
+{
+
+
+         tptr[i]->display();
+}
+        // tptr[1]->display();
+        // tptr[2]->display();
+        // tptr[3]->display();
+        // tptr[4]->display();
+for(int i=0; i<5; i++)
+{
+    delete tptr[i];
+    tptr[i]=NULL;
+}
+delete[]tptr;
+tptr=NULL;
+
+}
