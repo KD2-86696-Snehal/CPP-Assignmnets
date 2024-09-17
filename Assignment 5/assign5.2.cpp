099a59c00b3a902d8fdde9a760366bc18615ae18
snehalmathure <snehalmathure06@gmail.com>
2024-09-18 00:53:00 +0530

    Assignment 5 updated

---
+++ Assignment 5/assign5.2.cpp
@@ -0,0 +1,60 @@
+// Q2. Write a menu driven program for Student management.
+// Create a class student with data members name, gender, rollNumber(Auto generated) and array to 
+// keep marks of three subjects. 
+// Accept every thing from user and Print name, rollNumber, gender and percentage.
+// In main(), create Student* arr[5] and provide facility for accept, print, search.
+// Search function returns index of found Student, otherwise returns -1
+
+#include<iostream>
+using namespace std;
+class student{
+    string name;
+    string gender;
+    const int roll_no;
+    static int gener_rollno;
+    int mark[3];
+
+    public:
+    student():roll_no(++gener_rollno)
+    {
+        name="";
+        gender="";
+        for(int i=0;i<3;i++)
+        {
+            mark[i]=0;
+        }
+ 
+    }
+    student(string name, string gender)
+    {
+        this->name=name;
+        this->gender=gender;
+    }
+    void acceptstudents()
+    {
+        cout<<"Enter Student Name: ";
+        cin>>name;
+        cout<<"Enter gender- ";
+        cin>>gender;
+        cout<<"Enter Marks of Physics,Chemistry and maths: ";
+        for(int i=0; i<3; i++){
+            cin>>mark[i];
+        }
+    }
+    void displaystud()
+    {
+        cout<<"Name of the Student- "<<name<<endl;
+        cout<<"Gender of student- "<<gender<<endl;
+        cout<<"percentage- ";
+        percentage();
+    }
+    void percentage(){
+        int per,sum=0;
+        for(int i=0; i<3; i++)
+        {
+            sum+=mark[i];
+        }
+        per=(sum/300)*100;
+        cout<<per<<endl;
+    }
+};
