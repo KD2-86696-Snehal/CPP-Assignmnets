9e41e7d3f1940a96fa09d114a5357b9f8124f7a7
snehalmathure <snehalmathure06@gmail.com>
2024-09-19 14:46:14 +0530

    assignmnet 7 updated

--- Assignmnet 7/assign7.1.cpp
+++ Assignmnet 7/assign7.1.cpp
@@ -25,7 +25,7 @@ class product{
         this->title=title;
         this->price=price;
     }
-    void acceptproduct()
+    virtual void accept()
     {
         cout<<"ID- "<<endl;
         cin>>id;
@@ -35,11 +35,15 @@ class product{
         cin>>price;
 
     }
-    void displayproduct()
+    virtual void display()
     {
         cout<<"ID- "<<id<<endl;
         cout<<"Title- "<<title<<endl;
         cout<<"price- "<<price<<endl;
+    }
+    double getPrice()
+    {
+        return price;
     }
 
 };
@@ -55,15 +59,15 @@ class book: public product{
     {
         this->author=author;
     }
-    void acceptbook()
+    void accept()
     {
-        product::acceptproduct();
+        product::accept();
         cout<<"Enter Author name- ";
         cin>>author;
     }
-     void displaybook()
+     void dispaly()
     {
-        product::displayproduct();
+        product::display();
         cout<<"Author name- "<<author<<endl;
         
     }
@@ -82,23 +86,129 @@ class tape:public product{
     {
         this->artist=artist;
     }
-    void accepttape()
+    void accept()
     {
-        product::acceptproduct();
+        product::accept();
         cout<<"Enter Artist name- ";
         cin>>artist;
     }
-     void displayartist()
+     void display()
     {
-        product::displayproduct();
+        product::display();
         cout<<"Artist name- "<<artist<<endl;
         
     }
    
 };
+int menu()
+{
+    int choice;
+    cout << "1. Add BOOK" << endl;
+    cout << "2. Add Tape " << endl;
+    cout << "3.Display Books" <<endl;
+    cout << "4.Display Tapes" <<endl;
+    cout << "5.Calculate Discount" <<endl;
+    cout << "0. EXIT" << endl;
+    cout<<"Enter your choice: "<<endl;
+    cin>>choice;
+}
+    double calculatediscount(product **ptr)
+    {
+        double total=0;
+         for(int i=0; i<3; i++)
+            {
+                if(typeid (ptr[i])==typeid(book))
+                {
+                   book *bptr=(book *)ptr[i];
+                    double price=ptr[i]->getPrice();
+                    total=total+price*0.1;
+
+                }
+                else{
+                    for(int i=0; i<3; i++)
+                    {
+                
+                
+                   tape *tptr=(tape *)ptr[i];
+                    double price=ptr[i]->getPrice();
+                    total=total+price*0.05;
+
+                    }
+                }
+            }
+           return total; 
+    }
+
+
+
 int main()
 {
+    int choice;
+    double price=0;
     product *arr[3];
+    int index=0;
+    while((choice=menu())!=0)
+    {
+        switch(choice)
+        {
+            case 1:
+                if(index<3)
+                {
+                    arr[index]=new book;
+                    arr[index]->accept();
+                    index++;
+                }
+                else{
+                    cout<<"Book full"<<endl;
+                }
+                break;
+
+            case 2:
+
+                 if(index<3)
+                {
+                    arr[index]=new tape;
+                    arr[index]->accept();
+                    index++;
+                }
+                else{
+                    cout<<"tape full"<<endl;
+                }
+                break;
+            
+            case 3:
+            for(int i=0; i<3; i++){
+                if(typeid(arr[i])==typeid(book))
+                {
+                    arr[i]->display();
+                }
+                
+            }
+            break;
+            case 4:
+             for(int i=0; i<3; i++){
+                if(typeid(arr[i])==typeid(tape))
+                {
+                    arr[i]->display();
+                }
+                
+            }
+
+            case 5:
+          price= calculatediscount(arr);
+          cout<<"Total bill- "<<price;
+          break;
+          
+
+          case 0:
+          break;
+          default:
+          cout<<"Invalid choice"<<endl;
+          break;
+ 
+        }
+    }
+    
     
 }
 
